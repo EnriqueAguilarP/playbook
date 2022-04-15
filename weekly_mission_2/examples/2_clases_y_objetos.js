ca4cd7746f223c8891da1a52ac8883ef0156d60b
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-15 17:48:16 -0500

    métodos y uso de herencia

--- weekly_mission_2/examples/2_clases_y_objetos.js
+++ weekly_mission_2/examples/2_clases_y_objetos.js
@@ -1,18 +1,21 @@
 // Ejemplo 1: Crear una clase vacía
-class Person {
-}
 console.log("Ejemplo 1: Crear una clase vacía")
-console.log(Person) // [class Person]
+class Dron {
+}
+console.log(Dron) // [class Dron]
+console.log()
 
 // Ejemplo 2: Crear un objeto a partir de una clase
-class Pet {
-}
-const myPet1 = new Pet() // Puedo crear muchos objetos de la clase Pet
 console.log("Ejemplo 2: Crear un objeto a partir de una clase")
-console.log(myPet1) // un objeto de la clase Pet
+class Dron2 {
+}
+const droide = new Dron2() // Puedo crear muchos objetos de la clase
+console.log(droide) // un objeto de la clase Dron
+console.log()
 
 // Ejemplo 3: Instanciar un objeto con atributos
-class Student {
+console.log("Ejemplo 3: Instanciar un objeto con atributos")
+class Teacher {
   // El constructor nos permite instanciar un objeto y asignarle atributos, this nos ayuda a realizar esto.
   constructor(name, age, subjects){
     this.name = name
@@ -22,59 +25,62 @@ class Student {
 }
 
 // Crear un objeto de la clase Student (esto se le llama instanciación)
-const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"])
-console.log("Ejemplo 3: Instanciar un objeto con atributos")
-console.log(carloStudent)
+const enrique = new Teacher("Enrique", 31, ["POO", "Redes de computadoras"])
+console.log(enrique)
+console.log()
 
-// Ejemplo 4: Métodos en los objetos
-class Repository {
-  constructor(name, author, language, stars){
+// Ejemplo 4: Métodos en los +-
+console.log("Ejemplo 4: Métodos en los objetos")
+class Student0 {
+  constructor(name, age, subjects){
    this.name = name
-   this.author = author
-   this.language = language
-   this.stars = stars
+   this.age = age
+   this.subject = this.subject  
   }
 
   getInfo(){ // es una función que ejecutará cualquier objeto instanciado de esta clase
-    return `Repository ${this.name} has ${this.stars} stars`
+    return `Student ${this.name} has ${this.age} years old`
   }
 }
-console.log("Ejemplo 4: Métodos en los objetos")
-const myRepo = new Repository("LaunchX", "carlogilmar", "js", 100)
-console.log(myRepo.getInfo())
+
+const student1 = new Student0("Enrique Aguilar", 31, "POO")
+console.log(student1.getInfo())
+console.log()
 
 // Ejemplo 5: Atributos con valores por default
-class PullRequest {
-  constructor(repo, title, lines_changed){
-    this.repo = repo
-    this.title = title
-    this.lines_changed = lines_changed
-    this.status = "OPEN"
-    this.dateCreated = new Date() // esto guardará la fecha actual en que se instancia el objeto
+console.log("Ejemplo 5: Atributos con valores por default")
+class Student2 {
+  constructor(name, age){
+    this.name = name
+    this.age = age
+    this.subject = "Machine Learning"
+    this.language = "Python"
 	}
 
   getInfo(){
-    return `This PR is in the repo: ${this.repo} (status: ${this.status}) and was created on ${this.dateCreated}`
+    return `This student ${this.name} is enrolled in ${this.subject} with ${this.language}`
   }
 }
 
-console.log("Ejemplo 5: Atributos con valores por default")
-const myPR1 = new PullRequest("LaunchX", "Mi Primer PR", 100)
-console.log(myPR1.getInfo())
+
+const jeanStudent = new Student2("Jean", 30)
+console.log(jeanStudent.getInfo())
 
 // Puedes instanciar n cantidad de objetos de la misma clase
-const myPR2 = new PullRequest("LaunchX", "Mi segundo PR", 99)
-console.log(myPR2.getInfo())
+const anaStudent = new Student2("Ana", 35)
+console.log(anaStudent.getInfo())
+console.log()
 
 // Ejemplo 6: Getters para acceder a los atributos del objeto
+console.log("Ejemplo 6: Getters para acceder a los atributos del objeto")
 
-class Ajolonauta {
-  constructor(name, age, stack){
+class Explorer0 {
+  constructor(name, age){
    this.name = name
    this.age = age
-   this.stack = stack
-   this.exercises_completed = 0
-   this.exercises_todo = 99
+   this.stack = "Backend with NodeJS"
+   this.exercises_completed = 15
+   this.exercises_todo = 35
   }
 
   // Podemos acceder a los atributos de esta clase
@@ -83,17 +89,18 @@ class Ajolonauta {
   }
 }
 
-console.log("Ejemplo 6: Getters para acceder a los atributos del objeto")
-const woopa = new Ajolonauta("Woopa", 1, [])
-console.log(woopa.getExercisesCompleted)
+const enriqueAguilar = new Explorer0("Enrique Aguilar", 31)
+console.log(enriqueAguilar.getExercisesCompleted)
+console.log()
 
 // Ejemplo 7: Setters para modificar los atributos del objeto
+console.log("Ejemplo 7: Setters para modificar los atributos del objeto")
 class MissionCommander {
   constructor(name, mission){
 	  this.name = name
 	  this.mission = mission
-	  this.students = 0
-	  this.lives = 0
+	  this.students = 5
+	  this.lives = 4
   }
 
   get getStudents(){
@@ -113,38 +120,33 @@ class MissionCommander {
   }
 }
 
-console.log("Ejemplo 7: Setters para modificar los atributos del objeto")
-const missionCommanderNode = new MissionCommander("Carlo", "NodeJS")
+const missionCommanderNode = new MissionCommander("Carlo", "Backend")
 
-console.log(missionCommanderNode.getStudents) // 0 por default
-console.log(missionCommanderNode.getLives)// 0 por default
+console.log(missionCommanderNode.getStudents) 
+console.log(missionCommanderNode.getLives)
 
 // actualizamos los atributos por medio de los setters
 missionCommanderNode.setStudents = 100
 missionCommanderNode.setLives = 3
 
-console.log(missionCommanderNode.getStudents) // 0 por default
-console.log(missionCommanderNode.getLives)// 0 por default
+console.log(missionCommanderNode.getStudents)
+console.log(missionCommanderNode.getLives)
+console.log()
 
-// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto
+// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase 
+//que podemos usar sin necesidad de instanciar algún objeto
+console.log("Ejemplo 8: Métodos static")
 class Toolbox {
   static getMostUsefulTools(){
     return ["Command line", "git", "Text Editor"]
   }
 }
-
-console.log("Ejemplo 8: Métodos static")
 // Puedo llamar el método static directamente con el nombre de la clase
 console.log(Toolbox.getMostUsefulTools())
-// Si intentamos instanciar un objeto, no podremos llamar este método static
-//const toolbox = new Toolbox()
-//console.log(toolbox.getMostUsefulTools()) // is not a function
-
-/*
-HERENCIA: Nos permite relacionar clases entre sí y rehusar sus componentes
-*/
+console.log()
 
 // Ejemplo  9: Herencia entre dos clases
+console.log("Ejemplo  9: Herencia entre dos clases")
 class Developer {
   constructor(name, mainLang, stack){
     this.name =  name
@@ -157,20 +159,21 @@ class Developer {
 	}
 }
 
-console.log("Ejemplo  9: Herencia entre dos clases")
-const carloDev = new Developer("Carlo", "js", ["elixir", "groovy", "lisp"])
-console.log(carloDev)
+const enriqueDev = new Developer("Enrique", "javascript", "backend")
+console.log(enriqueDev)
 
 // Se usa la palabra extends para indicar que heredarás las propiedades de la clase Padre (Developer) en la clase definida.
 // Podemos definir una clase vacía y rehusar todos los componentes de la clase padre
 class LaunchXStudent extends Developer{
 }
 
-const carloLaunchXDev = new LaunchXStudent("Carlo", "js", ["elixir", "groovy", "lisp"])
-console.log(carloLaunchXDev)
-console.log(carloLaunchXDev.getName) // getter de la clase padre rehusada en la clase hija
+const enriqueLaunchXDev = new LaunchXStudent("Enrique", "javascript", "backend")
+console.log(enriqueLaunchXDev)
+console.log(enriqueLaunchXDev.getName) // getter de la clase padre rehusada en la clase hija
+console.log()
 
 // Ejemplo 10: Overrinding methods
+console.log("Ejemplo 10: Overrinding methods")
 
 class Explorer{
   constructor(name, username, mission){
@@ -197,9 +200,7 @@ class Viajero extends Explorer {
   }
 }
 
-const viajero1 = new Viajero("Carlo", "LaunchX", "Node JS", "Abril 2022")
-con
-sole.log("Ejemplo 10: Overrinding methods")
+const viajero1 = new Viajero("Enrique", "EnriqueAguilarP", "Node JS", "2022")
 console.log(viajero1)
 console.log(viajero1.getNameAndUsername()) // Método de la clase padre
 console.log(viajero1.getGeneralInfo()) // Método de la clase hija
