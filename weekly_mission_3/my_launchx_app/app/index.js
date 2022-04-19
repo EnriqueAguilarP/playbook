e84561106b5d7ec264e61831e0383b8a29db2cae
Enrique Aguilar Prado <99080355+EnriqueAguilarP@users.noreply.github.com>
2022-04-19 15:27:25 -0500

    Create index.js

---
+++ weekly_mission_3/my_launchx_app/app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('../app/missionCommander')
+const carloGilmar = new MissionCommander('Carlo Gilmar')
+const fernandaOchoa = new MissionCommander('Fernanda Ochoa')
+const rodrigoMartinez = new MissionCommander('Rodrigo Martinez')
+
+console.log(carloGilmar.name)
+console.log(fernandaOchoa.name)
+console.log(rodrigoMartinez.name)
