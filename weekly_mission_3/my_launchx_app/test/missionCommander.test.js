a6b79c8163209a6da2039e7b77b07b1672c274bd
Enrique Aguilar Prado <99080355+EnriqueAguilarP@users.noreply.github.com>
2022-04-19 17:32:09 -0500

    Update missionCommander.test.js

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('./../app/missionCommander')
 describe("Unit test for Mission Commander Class", () => {
     test('1) Create a mission commander object', () => {
       const myMissionCommander = new MissionCommander("Carlo Gilmar") 
-      expect(MyMissionCommander.name).toBe("Carlo Gilmar")
+      expect(myMissionCommander.name).toBe("Carlo Gilmar")
     })
   })
