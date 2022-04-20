6fad52de4f4361d6c2ff53f5040ca84e812aa778
Enrique Aguilar Prado <99080355+EnriqueAguilarP@users.noreply.github.com>
2022-04-20 09:01:38 -0500

    Create spiderman.test.js

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,17 @@
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit test for spiderman class", () => {
+    test('Create an spiderman object', () => {
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+      expect(andrewGarfield.name).toBe("Spiderman Sony")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.movies).toBe(2)
+      expect(andrewGarfield.studio).toBe("Sony")
+    });
+
+    test('Use the method getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+         });
+  })
